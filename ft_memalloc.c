b178735ca7b1cad7ee9911dc18ca1c3308e213bf
Mason Qian <mqian@e1z3r3p24.42.us.org>
2019-08-19 14:55:35 -0700

    quick update

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: mqian <mqian@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/07 17:27:53 by mqian             #+#    #+#             */
-/*   Updated: 2019/03/14 21:37:53 by mqian            ###   ########.fr       */
+/*   Updated: 2019/08/19 14:55:16 by mqian            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,11 @@
 void	*ft_memalloc(size_t size)
 {
 	void	*alloc;
-	int		i;
 
 	i = 0;
 	alloc = (void *)malloc(sizeof(char) * size);
 	if (!alloc)
-	{
 		return (NULL);
-	}
 	ft_bzero(alloc, size);
 	return (alloc);
 }
